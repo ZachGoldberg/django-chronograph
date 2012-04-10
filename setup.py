5454a74729065365802a76cf85ed309c912f9077
Steve Pulec <spulec@gmail.com>
2012-04-10 11:47:02 -0300

    Change version to 1.0

--- setup.py
+++ setup.py
@@ -3,28 +3,6 @@ from setuptools import setup, find_packages
 
 app_name = 'django-chronograph'
 
-def get_svn_revision(path=None):
-    rev = None
-    entries_path = '%s/.svn/entries' % path
-
-    if os.path.exists(entries_path):
-        entries = open(entries_path, 'r').read()
-        # Versions >= 7 of the entries file are flat text.  The first line is
-        # the version number. The next set of digits after 'dir' is the revision.
-        if re.match('(\d+)', entries):
-            rev_match = re.search('\d+\s+dir\s+(\d+)', entries)
-            if rev_match:
-                rev = rev_match.groups()[0]
-        # Older XML versions of the file specify revision as an attribute of
-        # the first entries node.
-        else:
-            from xml.dom import minidom
-            dom = minidom.parse(entries_path)
-            rev = dom.getElementsByTagName('entry')[0].getAttribute('revision')
-
-    if rev:
-        return u'svn-r%s' % rev
-    return u'svn-unknown'
 
 def fullsplit(path, result=None):
     """
@@ -66,7 +44,7 @@ for dirpath, dirnames, filenames in os.walk(src_dir):
 
 setup(
     name=app_name,
-    version=get_svn_revision(root_dir),
+    version=1.0,
     description='Django chronograph application.',
     author='Weston Nielson',
     author_email='wnielson@gmail.com',
