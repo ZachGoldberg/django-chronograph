4e02cad5f2bc34b7abca0941cd305da0f9415b04
Mingwei Gu <mingweigu@gmail.com>
2012-08-02 17:32:36 -0400

    Fix messaging: from django-chronograph commit 70b3667fa41da36fd73263e37898231bc7f683b4

--- chronograph/admin.py
+++ chronograph/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib import messages
 from django.db import models
 from django import forms
 from django.utils.translation import ungettext, ugettext_lazy as _
@@ -15,14 +16,14 @@ class HTMLWidget(forms.Widget):
     def __init__(self,rel=None, attrs=None):
         self.rel = rel
         super(HTMLWidget, self).__init__(attrs)
-    
+
     def render(self, name, value, attrs=None):
         if self.rel is not None:
             key = self.rel.get_related_field().name
             obj = self.rel.to._default_manager.get(**{key: value})
             related_url = '../../../%s/%s/%d/' % (self.rel.to._meta.app_label, self.rel.to._meta.object_name.lower(), value)
             value = "<a href='%s'>%s</a>" % (related_url, escape(obj))
-            
+
         final_attrs = self.build_attrs(attrs, name=name)
         return mark_safe("<div%s>%s</div>" % (flatatt(final_attrs), linebreaks(value)))
 
@@ -30,7 +31,7 @@ class JobAdmin(admin.ModelAdmin):
     list_display = ('name', 'next_run', 'last_run', 'frequency', 'params', 'get_timeuntil', 'is_running')
     list_filter = ('frequency', 'disabled',)
     ordering = ('name',)
-    
+
     fieldsets = (
         (None, {
             'fields': ('name', ('command', 'args',), 'disabled',)
@@ -40,7 +41,7 @@ class JobAdmin(admin.ModelAdmin):
             'fields': ('frequency', 'next_run', 'params',)
         }),
     )
-    
+
     def run_job_view(self, request, pk):
         """
         Runs the specified job.
@@ -50,9 +51,9 @@ class JobAdmin(admin.ModelAdmin):
         except Job.DoesNotExist:
             raise Http404
         job.run(save=False)
-        request.user.message_set.create(message=_('The job "%(job)s" was run successfully.') % {'job': job})        
+        messages.success(request, _('The job "%(job)s" was run successfully.') % {'job': job})
         return HttpResponseRedirect(request.path + "../")
-    
+
     def get_urls(self):
         urls = super(JobAdmin, self).get_urls()
         my_urls = patterns('',
@@ -73,28 +74,28 @@ class LogAdmin(admin.ModelAdmin):
             'fields': ('stdout', 'stderr',)
         }),
     )
-    
+
     def job_name(self, obj):
       return obj.job.name
     job_name.short_description = _(u'Name')
-    
+
     def has_add_permission(self, request):
         return False
-    
+
     def formfield_for_dbfield(self, db_field, **kwargs):
         request = kwargs.pop("request", None)
-        
+
         if isinstance(db_field, models.TextField):
             kwargs['widget'] = HTMLWidget()
             return db_field.formfield(**kwargs)
-        
+
         if isinstance(db_field, models.ForeignKey):
             kwargs['widget'] = HTMLWidget(db_field.rel)
             return db_field.formfield(**kwargs)
-        
+
         if isinstance(db_field, models.DateTimeField):
             print 'yup'
-        
+
         return super(LogAdmin, self).formfield_for_dbfield(db_field, **kwargs)
 
 admin.site.register(Job, JobAdmin)
